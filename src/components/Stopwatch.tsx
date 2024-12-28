051a03cf2dcc480bb449be4d9f2bcae6df7a43ee
sameerkulkarni27 <sameerk2114@gmail.com>
2024-12-27 17:55:40 -0800

    Stopwatch logic fully working

--- src/components/Stopwatch.tsx
+++ src/components/Stopwatch.tsx
@@ -3,15 +3,17 @@ import { useEffect, useState } from "react";
 function Stopwatch() {
     const [isRunning, setIsRunning] = useState(false);
     const [time, setTime] = useState(0);
+    const [start, setStart] = useState(0);
 
     useEffect(() => {
         let interval = null;
 
         if (isRunning) {
-            // Every sec
+            setStart(Date.now() - time);
+
             interval = setInterval(() => {
-                setTime((time) => time + 1);
-            }, 1);
+                setTime(Date.now() - start);
+            }, 10);
         };
 
         // Clear interval
@@ -21,7 +23,7 @@ function Stopwatch() {
             }
         };
 
-    }, [isRunning]);
+    }, [isRunning, start]);
 
     useEffect(() => {
         const spacePressed = (event: KeyboardEvent) => {
@@ -56,13 +58,13 @@ function Stopwatch() {
     }, [isRunning, time]);
 
     const formatTime = () => {
-        const milliseconds = time % 1000;
-        const seconds = Math.floor(time / 1000);
-        const minutes = Math.floor(seconds / 60);
+        const milliseconds = Math.floor(time % 1000);
+        const seconds = Math.floor(time / 1000) % 60;
+        const minutes = Math.floor(time / 60000) % 60;
 
-        const millisecondsText = (milliseconds / 10) < 10 ? `0${Math.floor(milliseconds / 10)}` : `${Math.floor(milliseconds / 10)}`;
-        const secondsText = (seconds % 60) < 10 ? `0${seconds % 60}` : `${seconds % 60}`;
-        const minutesText = minutes;
+        const millisecondsText = ("0" + milliseconds).slice(-2);
+        const secondsText = ("0" + seconds).slice(-2);
+        const minutesText = ("0" + minutes).slice(-2);
 
         return `${minutesText}:${secondsText}:${millisecondsText}`;
     }
